84ee8ec2dbd61d94085296a3a06009aba3c4b65e
村上奈緒美 <murakami.naomi@a-tm.co.jp>
2021-10-15 20:17:52 +0900

    todoを取得・追加・更新・削除するための関数を作成

---
+++ src/api/todos.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const todoDataUrl = "http://localhost:3100/todos";
+
+//全部のtodoを取得する
+export const getAllTodosData = async () => {
+  const response = await axios.get(todoDataUrl);
+  return response.data;　// objectとして dataがかえってくる
+}
+
+//一つのtodoを返すので単数系
+export const addTodoData = async (todo) => {
+  const response = await axios.post(todoDataUrl, todo);
+  return response.data;//新しい配列を追加した分が帰ってくる　新しいTodoListを追加したイメージ
+}
+
+//削除
+export const deleteTodoData = async (id) => {
+  await axios.delete(`${todoDataUrl}` / `${id}`)
+  return id;
+}
+
+//更新
+export const updateTodoData = async (id, todo) => {
+  const response = await axios.put(`${todoDataUrl}` / `${id}`, todo);
+  return response.data;
+}
+
+//getAllTodosData();で呼び出す
